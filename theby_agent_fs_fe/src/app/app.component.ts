88dc017f7c59cf139be477bb327dfc247b6085bb
Paul Theby <paultheby@gmail.com>
2024-02-24 14:30:07 -0600

    Basic Sign/Login Style

--- theby_agent_fs_fe/src/app/app.component.ts
+++ theby_agent_fs_fe/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { UserSignupComponent } from './auth/pages/user-signup/user-signup.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserSignupComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
