437ec0d21d03db342a81f6863b0bd3c4f6d41a25
Paul Theby <paultheby@gmail.com>
2024-02-25 18:39:40 -0600

    Updated agent model
    need to see how the user and group id are represented if at all on the angular side.

--- theby_agent_fs_fe/src/app/shared/models/agent.ts
+++ theby_agent_fs_fe/src/app/shared/models/agent.ts
@@ -1,17 +1,19 @@
 import { NumberValueAccessor } from "@angular/forms";
+import { User } from "./user";
 
 export class Agent {
   firstName:string;
   lastName:string;
   email:string;
   npn: number;
-  user_id?:number;
-  group_id? :number;
+  user?:any;
+  group? :any;
 
-  constructor (firstName:string,lastName:string,email:string, npn: number){
+  constructor (firstName:string,lastName:string,email:string, npn: number,user:User,group:Group){
 this.firstName= firstName;
 this.lastName= lastName;
 this.email= email;
 this.npn=npn
+this.user=user;
    }
 }
