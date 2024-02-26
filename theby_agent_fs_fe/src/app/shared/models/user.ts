b29f73d84f25b0b63d404136aae079cd3fd10274
Paul Theby <paultheby@gmail.com>
2024-02-25 18:25:26 -0600

    Unsure what this is

---
+++ theby_agent_fs_fe/src/app/shared/models/user.ts
@@ -0,0 +1,14 @@
+export class User {
+  id?: number ;
+  email: string;
+  password:string;
+  role:string;
+
+constructor(email:string,password:string,role:string) {
+
+ this.email = email;
+ this.password = password;
+ this.role =role
+
+}
+}
